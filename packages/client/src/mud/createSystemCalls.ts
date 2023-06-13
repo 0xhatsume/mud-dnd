90998fb282daa95f25180bad44b0f123a39e6a12
0xhatsume <0xhatsume@gmail.com>
2023-06-13 09:33:35 +0800

    feat: update client mud system calls

--- packages/client/src/mud/createSystemCalls.ts
+++ packages/client/src/mud/createSystemCalls.ts
@@ -7,15 +7,48 @@ export type SystemCalls = ReturnType<typeof createSystemCalls>;
 
 export function createSystemCalls(
   { worldSend, txReduced$, singletonEntity }: SetupNetworkResult,
-  { Counter }: ClientComponents
+  { Counter, Stories }: ClientComponents
 ) {
+
   const increment = async () => {
     const tx = await worldSend("increment", []);
     await awaitStreamValue(txReduced$, (txHash) => txHash === tx.hash);
     return getComponentValue(Counter, singletonEntity);
   };
 
+  interface Story {
+    name:string, dm_system_message:string, 
+    questspecifics:string,
+    user_description:string, game_description:string,
+    //picture_url: string
+  }
+  const createStory = async ({
+    name, 
+    dm_system_message, 
+    questspecifics, 
+    user_description, 
+    game_description,
+    //picture_url
+    }: Story) => {
+    
+    const tx = worldSend("createStory", [
+      name, dm_system_message, 
+      questspecifics, user_description, 
+      game_description,
+      //picture_url
+    ])
+    await awaitStreamValue(txReduced$, (txHash) => txHash === tx.hash);
+    return true;
+  };
+
+  const addToDo = async (body: string) => {
+    const tx = await worldSend("addToDo", [body]);
+    return true;
+  }
+
   return {
     increment,
+    createStory,
+    addToDo
   };
 }
